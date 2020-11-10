1b76ce4ebba86b4eaee5f75951e13bb404303480
wesley guerra <guerra_ssa@hotmail.com>
2020-11-10 13:08:54 -0300

    Criptografia de senha

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,12 +1,15 @@
 import { getRepository } from 'typeorm';
 import User from '../models/Users';
+import { hash } from 'bcryptjs';
 
 interface Request {
     name: string, email: string, password: string;
 }
 
 class CreateUserSerive {
+
     public async execute({ name, email, password }: Request): Promise<User> {
+
         const usersRepository = getRepository(User);
 
         const checkUserExist = await usersRepository.findOne({
@@ -17,11 +20,15 @@ class CreateUserSerive {
             throw new Error('Email address already used by another');
         }
 
+        const hasgedPassword = await hash(password, 8)
+
         const user = usersRepository.create({
             name,
             email,
-            password
+            password: hasgedPassword,
         });
+
+        delete user.password;
 
         await usersRepository.save(user)
 
