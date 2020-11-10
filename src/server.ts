2b444c162e08f4925acfbd6067d67b2ee8a7e03e
wesley guerra <guerra_ssa@hotmail.com>
2020-11-09 22:11:11 -0300

    criação de conexão com banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json()); //TRABALHANDO COM JSON
 app.use(routes); //ROTAS
-
-
 
 app.listen('3333', () => {
     console.log('Server start on port 3333');
