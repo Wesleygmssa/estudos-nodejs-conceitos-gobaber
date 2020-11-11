5e9140586fe6f695e0567f2c574931b149ecb759
wesley guerra <guerra_ssa@hotmail.com>
2020-11-10 21:53:49 -0300

    servindo arquivo estático

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 import './database'; // importando conexão database 
 
 const app = express();
 app.use(express.json()); // utlizando formato json sempre acima das rotas
 app.use(routes);// ordem linear 
+app.use('/file', express.static(uploadConfig.directory)); //exibir fotos localhost
 
-app.listen(3333,()=>{
+app.listen(3333, () => {
     console.log('🚀Server started on port 3333! 🚀')
 })
