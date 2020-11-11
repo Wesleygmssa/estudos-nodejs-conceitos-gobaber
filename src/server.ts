e435907cf50263c723e0605afc707da7e5a9c938
wesley guerra <guerra_ssa@hotmail.com>
2020-11-11 09:50:48 -0300

    lidando com erros

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,36 @@
 import 'reflect-metadata';
+
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 import './database'; // importando conexão database 
 
 const app = express();
-app.use(express.json()); // utlizando formato json sempre acima das rotas
-app.use('/file', express.static(uploadConfig.directory)); //exibir fotos localhost
+app.use(express.json()); // Utl.JSON
+app.use('/file', express.static(uploadConfig.directory)); //exibir fotos localhost:
 app.use(routes);// ordem linear 
-//trativa de erro
-// app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-//     if()
-// });
+
+//trativa de erro, captando todo erros, centralizando 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    };
+
+    //erro não esperado
+    // console.log(err)
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    });
+});
 
 
 app.listen(3333, () => {
-    console.log('🚀Server started on port 3333! 🚀')
-})
+    console.log('🚀Server started on port 3333! 🚀');
+});
